8b3da225fe96d237b0ee2f047ae86654396d87d5
haim1993 <haim.schlesinger@logz.io>
2019-11-21 17:01:49 +0200

    [+] added Jetty compability and replica in the haproxy with load balancing

---
+++ logs-shipper/src/main/java/com/Resource.java
@@ -0,0 +1,25 @@
+package com;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.UUID;
+
+@Path("boot-bootcamp")
+public class Resource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sendLog() {
+        String response = "Seed: " + SimpleServer.SEED + ", Number of visits: " + (SimpleServer.VISITS_COUNTER++);
+
+        Logger logger = LogManager.getLogger(Resource.class);
+        logger.info(response);
+
+        return response;
+    }
+
+}
---
+++ logs-shipper/src/main/java/com/SimpleServer.java
@@ -0,0 +1,44 @@
+package com;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+
+public class SimpleServer {
+    public static int PORT = 8080;
+    public static String SEED;
+
+    public static int VISITS_COUNTER = 1;
+
+    public static void main(String[] args) throws IOException {
+        // Generate unique random seed
+        SEED = UUID.randomUUID().toString();
+
+        ResourceConfig config = new ResourceConfig();
+        config.packages("com");
+        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+
+        Server server = new Server(PORT);
+        ServletContextHandler context = new ServletContextHandler(server, "/*");
+        context.addServlet(servlet, "/*");
+
+        try {
+            server.start();
+            server.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            server.destroy();
+        }
+    }
+}
