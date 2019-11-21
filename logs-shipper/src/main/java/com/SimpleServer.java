8b58c52035b997c4f950167d41bbb2eb607f4b72
haim1993 <haim.schlesinger@logz.io>
2019-11-21 18:39:11 +0200

    [+] WORKING STATE - REPLICAS WITH HAPROXY

--- logs-shipper/src/main/java/com/SimpleServer.java
+++ logs-shipper/src/main/java/com/SimpleServer.java
@@ -3,8 +3,6 @@ package com;
 import java.io.IOException;
 import java.util.UUID;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
